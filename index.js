86d6eb1939df6ed4108caba1b400253e6123ec5c
Daniel Kimani <daniel.kibunga@students.moringaschool.com>
2025-03-17 11:39:06 +0300

    added styling to the form

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const ramens = [
-    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
-    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
-    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" },
- 
- 
-    { 
-        name: "Shoyu Ramen", 
-        restaurant: "Ichiran", 
-        image: "/shoyu.jpg", 
-        rating: 5, 
-        comment: "Delicious!" 
-    },]
-
-   
+    { id: 1, name: "Gyukotsu Ramen", restaurant: "Ichiran", image: "gyukotsu.jpg", rating: 5, comment: "Rich and flavorful!" },
+    { id: 2, name: "Kojiro Ramen", restaurant: "Menya", image: "kojiro.jpg", rating: 4, comment: "Great taste and texture!" },
+    { id: 3, name: "Naruto Ramen", restaurant: "Ramen Ichiban", image: "naruto.jpg", rating: 5, comment: "Perfect balance of flavors!" },
+];
+function displayRamens (){
+    
+}
